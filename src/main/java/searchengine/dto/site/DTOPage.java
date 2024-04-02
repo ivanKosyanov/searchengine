a42f93867f1c1e417424a2196fd41f2c9d18768e
Asus_TUF <cosyanov.ivan@yandex.ru>
2024-04-02 20:17:52 +0300

    not done finalProject

---
+++ src/main/java/searchengine/dto/site/DTOPage.java
@@ -0,0 +1,13 @@
+package searchengine.dto.site;
+
+import lombok.Data;
+
+@Data
+public class DTOPage {
+    private String site;
+    private String siteName;
+    private String uri;
+    private String tittle;
+    private String snippet;
+    private float relevance;
+}
